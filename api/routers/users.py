3945151dce2785d011781229f6869e723305e9d8
junhanan@gmail.com <junhanan@gmail.com>
2023-06-29 13:00:43 -0400

    auth protec push while i'm crying

--- api/routers/users.py
+++ api/routers/users.py
@@ -15,7 +15,7 @@ from pydantic import BaseModel
 from models import (
     UserIn,
     UserOut,
-    # User,
+    User,
     DuplicateUserError,
 )
 
@@ -38,6 +38,26 @@ class HttpError(BaseModel):
 router = APIRouter()
 
 
+@router.get("/api/protected", response_model=bool)
+async def get_protected(
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    return True
+
+
+@router.get("/token", response_model=AccountToken | None)
+async def get_token(
+    request: Request,
+    account: UserOut = Depends(authenticator.try_get_current_account_data),
+) -> AccountToken | None:
+    if account and authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "account": account,
+        }
+
+
 @router.post("/api/users", response_model=AccountToken | HttpError)
 async def create_user(
     info: UserIn,
@@ -57,23 +77,3 @@ async def create_user(
     token = await authenticator.login(response, request, form, users)
     return AccountToken(account=account, **token.dict())
 
-
-# @router.post("/api/users", response_model=AccountToken | HttpError)
-# async def create_user(
-#     info: UserIn,
-#     request: Request,
-#     response: Response,
-#     users: UserQueries = Depends(),
-# ):
-#     hashed_password = authenticator.hash_password(info.password)
-#     try:
-#         account_data = users.create_user(info.dict(), hashed_password)
-#         account = User(**account_data)
-#     except DuplicateUserError:
-#         raise HTTPException(
-#             status_code=status.HTTP_400_BAD_REQUEST,
-#             detail="Cannot create an account with those credentials",
-#         )
-#     form = AccountForm(username=info.email, password=info.password)
-#     token = await authenticator.login(response, request, form, users)
-#     return AccountToken(account=account, **token.dict())
