66bbea6f2edf88070eadba6436d2b651fc23515b
Tim <tfdeleon@buffalo.edu>
2023-07-10 12:45:11 -0400

    login form

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -1,18 +1,18 @@
-steps = [
-    [
-        # "Up" SQL statement
-        """
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY NOT NULL,
-            first VARCHAR(100) NOT NULL,
-            last VARCHAR(100) NOT NULL,
-            mbti VARCHAR(4) NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL
-        );
-        """,
-        # "Down" SQL statement
-        """
-        DROP TABLE users;
-        """,
-    ],
-]
+# steps = [
+#     [
+#         # "Up" SQL statement
+#         """
+#         CREATE TABLE users (
+#             id SERIAL PRIMARY KEY NOT NULL,
+#             first VARCHAR(100) NOT NULL,
+#             last VARCHAR(100) NOT NULL,
+#             mbti VARCHAR(4) NOT NULL,
+#             email VARCHAR(255) UNIQUE NOT NULL
+#         );
+#         """,
+#         # "Down" SQL statement
+#         """
+#         DROP TABLE users;
+#         """,
+#     ],
+# ]
--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,36 +1,16 @@
-import { useEffect, useState } from "react";
-import Construct from "./Construct.js";
-import ErrorNotification from "./ErrorNotification";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "./login.js";
 
-function App() {
-  const [launchInfo, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
-      console.log("fastapi url: ", url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, []);
-
+function App(props) {
   return (
-    <div>
-      <ErrorNotification error={error} />
-      <Construct info={launchInfo} />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/login" element={<login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
