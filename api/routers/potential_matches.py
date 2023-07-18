22d7ecd3ef8d8aef043dea9984208d650c39b33d
junhanan@gmail.com <junhanan@gmail.com>
2023-07-18 11:24:20 -0400

    changed user_id to matched_user

--- api/routers/potential_matches.py
+++ api/routers/potential_matches.py
@@ -5,7 +5,7 @@ from db.potential_matches_db import (
     PotentialMatch,
     PotentialMatchIn,
     PotentialMatchOut,
-    PotentialMatchQueries
+    PotentialMatchQueries,
 )
 from db.user_db import UserOut, UserQueries
 
@@ -24,7 +24,7 @@ def create_potential_match(
     logged_in_user = user_queries.get_user_by_id(
         potential_match.logged_in_user
     )
-    match_user = user_queries.get_user_by_id(potential_match.user_id)
+    match_user = user_queries.get_user_by_id(potential_match.matched_user)
 
     if not logged_in_user or not match_user:
         raise HTTPException(status_code=404, detail="User not found.")
@@ -52,9 +52,15 @@ def get_potential_matches(user_id: int) -> List[PotentialMatchOut]:
 
 @router.put("/potential-matches/{match_id}", response_model=PotentialMatchOut)
 async def update_potential_match(match_id: int, liked: bool):
-    potential_match = potential_match_queries.get_potential_matches_by_user(match_id)
+    potential_match = potential_match_queries.get_potential_matches_by_user(
+        match_id
+    )
     if not potential_match:
-        raise HTTPException(status_code=404, detail="Potential match not found")
+        raise HTTPException(
+            status_code=404, detail="Potential match not found"
+        )
 
-    updated_match = potential_match_queries.update_potential_match(match_id, liked)
+    updated_match = potential_match_queries.update_potential_match(
+        match_id, liked
+    )
     return updated_match
