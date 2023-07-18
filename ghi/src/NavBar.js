401fc19d7df4bdfc166ee3c80702924dab33e7ea
Jay <santana.jazel423@gmail.com>
2023-07-18 11:15:56 -0400

    added profile card and styling

--- ghi/src/NavBar.js
+++ ghi/src/NavBar.js
@@ -8,24 +8,25 @@ export default function Navbar() {
             <div className="container-fluid mx-auto px-0 justify-between">
                 <div className="flex space-x-4">
                     <NavLink to="/" className="text-white text-base font-bold px-4 py-2 hover:bg-pink-600 rounded flex items-center">
-                        <svg className="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-heart-fill" viewBox="0 0 16 16">
+                            <path d="M7.293 1.5a1 1 0 0 1 1.414 0L11 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l2.354 2.353a.5.5 0 0 1-.708.707L8 2.207 1.354 8.853a.5.5 0 1 1-.708-.707L7.293 1.5Z" />
+                            <path d="m14 9.293-6-6-6 6V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9.293Zm-6-.811c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.691 0-5.018Z" />
                         </svg>
                         Home
                     </NavLink>
                 </div>
                 <div className="flex space-x-4">
                     <Link href="#" className="text-white text-base font-bold px-4 py-2 hover:bg-pink-600 rounded flex items-center">
-                        <svg className="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-through-heart-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.854 15.854A.5.5 0 0 1 2 15.5V14H.5a.5.5 0 0 1-.354-.854l1.5-1.5A.5.5 0 0 1 2 11.5h1.793l3.103-3.104a.5.5 0 1 1 .708.708L4.5 12.207V14a.5.5 0 0 1-.146.354l-1.5 1.5ZM16 3.5a.5.5 0 0 1-.854.354L14 2.707l-1.006 1.006c.236.248.44.531.6.845.562 1.096.585 2.517-.213 4.092-.793 1.563-2.395 3.288-5.105 5.08L8 13.912l-.276-.182A23.825 23.825 0 0 1 5.8 12.323L8.31 9.81a1.5 1.5 0 0 0-2.122-2.122L3.657 10.22a8.827 8.827 0 0 1-1.039-1.57c-.798-1.576-.775-2.997-.213-4.093C3.426 2.565 6.18 1.809 8 3.233c1.25-.98 2.944-.928 4.212-.152L13.292 2 12.147.854A.5.5 0 0 1 12.5 0h3a.5.5 0 0 1 .5.5v3Z" />
                         </svg>
                         Matches
                     </Link>
                 </div>
                 <div className="flex space-x-4">
                     <a href="#" className="text-white text-base font-bold px-4 py-2 hover:bg-pink-600 rounded flex items-center">
-                        <svg className="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 4H4a1 1 0 00-1 1v14a1 1 0 001 1h16a1 1 0 001-1V5a1 1 0 00-1-1h-6l-2-2m-4 0L5 4h14zm-9 6h4m0 4h4m-4-8h4m0 4h4m-4 4h4m0-8h4m0 12H5v-6h4v2zm6 0h4v-2h-4v2zm0-4v-2h4v2h-4zm0 4h4v-2h-4v2z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+                            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
                         </svg>
                         Profile
                     </a>
--- ghi/src/PotentialMatches/ProfileCard.js
+++ ghi/src/PotentialMatches/ProfileCard.js
@@ -3,31 +3,49 @@ import PropTypes from 'prop-types';
 //import { Test } from './ProfileCard.styles';
 
 const ProfileCard = (props) => (
-    <div style={{ width: '300px', margin: '0 auto' }}>
+    <div
+        style={{
+            width: "300px",
+            margin: "0 auto",
+            boxShadow: "5px 5px 15px rgb(255, 175, 204)",
+            transition: "0.3s",
+            borderRadius: '30px',
+            overflow: 'hidden'
+        }}
+    >
         <div className="card">
             <img
                 src={props.image}
                 className="card-img-top"
                 alt="Profile"
                 style={{
-                    width: "100%", // or any other size
-                    height: "auto" // or any other size
+                    width: "100%",
+                    height: "auto"
                 }}
             />
-            <div className="card-body">
-                <h5 className="card-title">{props.name}</h5>
-                <p className="card-text">{props.bio}</p>
+            <div style={{ padding: "10px 0 0 4px" }}>
+                <h2 style={{ fontSize: "24px", marginBottom: "10px" }}>{props.name}</h2>
             </div>
             <div
                 className="card-footer"
-            // style={{ display: "flex", justifyContent: "space-between" }}
+                style={{
+                    display: "flex",
+                    // justifyContent: "space-between",
+                    fontSize: "18px",
+                    // borderTop: "1px solid #ddd",
+                    paddingTop: "10px",
+                    padding: '4px'
+                }}
             >
-                <small className="text-muted">{props.age} - </small>
-                <small className="text-muted">{props.city}, {props.state}</small>
+                <h5 className="text-muted">{props.age} - </h5>
+                <h5 className="text-muted">
+                    {props.city}, {props.state}
+                </h5>
             </div>
         </div>
     </div>
 );
+
 
 ProfileCard.propTypes = {
     name: PropTypes.string.isRequired,
