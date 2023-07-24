25effe361abdf15bf3a28137db53685c03f095b8
junhanan@gmail.com <junhanan@gmail.com>
2023-07-24 15:03:07 -0400

    adding bootstrap deleted nav fixed app.js

--- ghi/run.sh
+++ ghi/run.sh
@@ -1,5 +1,6 @@
 npm install react-router-dom --save
 npm install @galvanize-inc/jwtdown-for-react --save
 npm install date-fns --save
+npm install react-bootstrap bootstrap --save
 npm update
 npm start
--- ghi/src/App.js
+++ ghi/src/App.js
@@ -4,59 +4,21 @@ import SignupForm from "./Account/SignUpForm";
 import Construct from "./Construct.js";
 import ErrorNotification from "./ErrorNotification";
 import "./App.css";
-// import { getCurrentUserId } from "./auth";
-// import { login, logout } from "./auth";
 import LoginForm from "./Account/LoginForm";
-// import LogOut from "./LogOut";
-// import InterestsForm from "./InterestsForm";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PotentialMatches from './PotentialMatches/PotentialMatches';
 import ProfilePage from './ProfilePage';
-// import PotentialMatches from './PotentialMatches/bitchass';
-// import PotentialMatches from './PotentialMatches/likepotentialmatch';
-// import SignupForm from './Account/mocksignup'
 import Chat from './Account/chat';
 import NavbarComponent from './NavBar';
 import Nav from './Nav.js';
-import ProfilePage from './ProfilePage';
+
 
 
 
 
 function App() {
-
-  // const domain = /https:\/\/[^/]+/;
-  // // const baseURL = process.env.REACT_APP_PUBLIC_URL.replace(domain, "");
   const baseURL = process.env.REACT_APP_API_HOST
-
-  // const [launchInfo, setLaunchInfo] = useState([]);
-  // const [error, setError] = useState(null);
-  // const [userId, setUserId] = useState(null);
-
-
-  // useEffect(() => {
-  //   // const id = getCurrentUserId();
-  //   // setUserId(id);
-
-  //   async function getData() {
-  //     // let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
-  //     let url = `${baseURL}/api/launch-details`;
-  //     console.log("fastapi url: ", url);
-  //     let response = await fetch(url);
-  //     console.log("------- hello? -------");
-  //     let data = await response.json();
-
-  //     if (response.ok) {
-  //       console.log("got launch data!");
-  //       setLaunchInfo(data.launch_details);
-  //     } else {
-  //       console.log("drat! something happened");
-  //       setError(data.message);
-  //     }
-  //   }
-  //   getData();
-  // }, []);
 
   return (
     <div className="">
