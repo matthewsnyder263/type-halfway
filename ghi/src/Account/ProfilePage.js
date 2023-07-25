6a1bd8762b7462e031ddaf74bb4fd485bf1858a1
Tim <tfdeleon@buffalo.edu>
2023-07-25 15:22:36 -0400

    working copy of a profile page, need routing for matched users

--- ghi/src/Account/ProfilePage.js
+++ ghi/src/Account/ProfilePage.js
@@ -113,7 +113,7 @@ const ProfilePage = () => {
                             <div className="media align-items-end profile-head">
                                 <div className="profile mr-3">
                                     <img
-                                        src={currentUser.picture}
+                                        src="https://picsum.photos/200"
                                         alt="..."
                                         // width="130"
                                         style={{
@@ -135,9 +135,11 @@ const ProfilePage = () => {
                                 {/* SNYDER CODE ADDED ABOVE */}
 
                                 <div className="media-body mb-5 text-white">
-                                    <h4 className="mt-0 mb-0">{currentUser.username}</h4>
-                                    <h4 className="mt-0 mb-0">{currentUser.age}</h4>
-                                    <h4 className="mt-0 mb-0">{currentUser.mbti}</h4>
+                                    <h4 className="mt-0 mb-0"> Name: {currentUser.username}</h4>
+                                    <h4 className="mt-0 mb-0"> Age: {currentUser.age}</h4>
+                                    <h4 className="mt-0 mb-0">MBTI: {currentUser.mbti}</h4>
+                                    <h4 className="mt-0 mb-0">Gender: {currentUser.gender}</h4>
+                                    <h4 className="mt-0 mb-0">Interests: {currentUser.interest}</h4>
                                     <p className="small mb-4">
                                         <i className="fas fa-map-marker-alt mr-2"></i>
                                         {currentUser.city}, {currentUser.state}
@@ -147,13 +149,19 @@ const ProfilePage = () => {
                         </div>
 
                         <div className="px-4 py-3">
-                            <h5 className="mb-0">A little about me...</h5>
+                            <h5 className="mb-0">Bio:</h5>
                             <div className="p-4 rounded shadow-sm bg-light">
                                 <p className="font-italic mb-0">{currentUser.bio}</p>
                             </div>
                         </div>
+                        <div>
+                            <button className="btn btn-outline-light btn-sm btn-block">
+                                View My Likes
+                            </button>
 
-                        <div className="py-4 px-4">
+                        </div>
+
+                        {/* <div className="py-4 px-4">
                             <div className="d-flex align-items-center justify-content-between mb-3">
                                 <h5 className="mb-0">Recent photos</h5>
                                 <Link to="#" className="btn btn-link text-muted">
@@ -176,7 +184,7 @@ const ProfilePage = () => {
                                 alt=""
                                 className="img-fluid rounded shadow-sm"
                             />
-                        </div>
+                        </div> */}
 
                     </div>
                 </div>
