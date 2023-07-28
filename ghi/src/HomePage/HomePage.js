276b58ea3542ca403557df8b38f4275987c76ad9
Matt Snyder <msnyd87@gmail.com>
2023-07-28 14:19:15 -0500

    HomePage Asian Girl Compat Link and Calculator Working

--- ghi/src/HomePage/HomePage.js
+++ ghi/src/HomePage/HomePage.js
@@ -52,7 +52,7 @@ const One = () => {
                         Join our platform today and embark on a journey that transcends typical dating experiences. Discover the magic of scientific MBTI compatibility, and find your perfect match based on authentic connections, shared values, and genuine understanding. We are dedicated to making your search for love not only enjoyable but also accurate and deeply rewarding. Let's redefine dating together!
                         <div>Click the link below to check your compatibility</div>
                     </p>
-                    <Link to="#" className="special">
+                    <Link to="/compatibilitycalculator" className="special">
                         Check your compatibility level
                     </Link>
                 </div>
